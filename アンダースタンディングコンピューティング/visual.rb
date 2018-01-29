0aa58cc111662736ef9b6514654ca6fd2833ea32
katayama <katayama@localdomain>
2018-01-29 13:59:44 +0900

    進んだものをコミット

--- アンダースタンディングコンピューティング/visual.rb
+++ アンダースタンディングコンピューティング/visual.rb
@@ -188,6 +188,10 @@ class Assign < Struct.new(:name ,:expression)
     def evaluate(environment)
         environment.merge({ name => expression.evaluate(environment)})
     end
+
+    def to_ruby
+        "-> e {e.merge({ #{name.inspect} => (#{expression.to_ruby}).call(e)})} "
+    end
 end
 
 class DoNothing
@@ -207,6 +211,10 @@ class DoNothing
 
     def evaluate(environment)
         environment
+    end
+
+    def to_ruby
+        '-> e { e }'
     end
 end
 
@@ -234,7 +242,7 @@ class If < Struct.new(:condition, :consequence, :alternative)
                 [alternative, environment]
             end
         end
-		end
+	end
 		
 		def evaluate(environment)
 			case condition.evaluate(environment)
@@ -243,7 +251,14 @@ class If < Struct.new(:condition, :consequence, :alternative)
 			when Boolean.new(false)
 				alternative.evaluate(environment)
 			end
-		end
+        end
+
+    def to_ruby
+        "-> e { if (#{condition.to_ruby}).call(e)" +
+          "then (#{consequence.to_ruby}).call(e)" +
+          "else (#{alternative.to_ruby}).call(e)" +
+          "end }"
+    end
 end
 
 class Sequence < Struct.new(:first, :second)
@@ -271,7 +286,11 @@ class Sequence < Struct.new(:first, :second)
 
 	def evaluate(environment)
 		second.evaluate(first.evaluate(environment))
-	end
+    end
+    
+    def to_ruby
+        "-> e { (#{second.to_ruby}).call((#{first.to_ruby}).call(e)) }"
+    end
 end
 
 class While < Struct.new(:condition, :body)
@@ -298,7 +317,14 @@ class While < Struct.new(:condition, :body)
 		when Boolean.new(false)
 			environment
 		end
-	end
+    end
+    
+    def to_ruby
+        "-> e {" +
+          "while (#{condition.to_ruby}).call(e); e = (#{body.to_ruby}).call(e); end;" +
+          " e" +
+          "}"
+    end
 end
 
 # Number.new(23).evaluate({})
@@ -316,3 +342,13 @@ end
 # LessThan.new(Add.new(Variable.new(:x), Number.new(1)), Number.new(3)).to_ruby
 # proc = eval(LessThan.new(Add.new(Variable.new(:x),Number.new(1)),Number.new(3)).to_ruby)
 # proc.call(environment)
+
+# statement = Assign.new(:y, Add.new(Variable.new(:x), Number.new(1)))
+# statement.to_ruby
+# proc = eval(statement.to_ruby)
+# proc.call({x: 3})
+
+# statement = While.new(LessThan.new(Variable.new(:x), Number.new(5)),Assign.new(:x,Multiply.new(Variable.new(:x), Number.new(3))))
+# statement.to_ruby
+# proc = eval(statement.to_ruby)
+# proc.call({x:1})
